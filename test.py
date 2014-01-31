08892cf9778d754f12160f5e49ea00348ac2497d
Erik Gartz <eegunnar@yahoo.com>
2014-01-30 21:27:57 -0500

    have a basic test passing

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import subprocess
 import shutil
+import sys
 
 cwd = os.getcwd()
  
@@ -17,21 +18,25 @@ class MyTest(unittest.TestCase):
         self.assertEqual(1,1, 'testing 1 2 3')
     
     def testFirstRun(self):
-        ''' Test first time program run with nothing to do.'''
+        ''' Test with nothing to do (2 runs).'''
 
         config_dict = {
             'base_folder': cwd + 'test/testFirstRun/base/tmp',
             'target_folder': cwd + 'test/testFirstRun/target/tmp'
         }
         result = run_program(config_dict)
+        self.assertEqual(result['ret_val'], 0, 'first empty run ok')
 
+        result = run_program(config_dict)
+        self.assertEqual(result['ret_val'], 0, 'second empty run ok')
         
-def run_program(config_dict, clean_base = True, clean_target = True):
+def run_program(config_dict, clean_base = True, clean_target = True, print_failed_run = True):
     for manditory_param in ('base_folder', 'target_folder'):
         if manditory_param not in config_dict:
             raise Exception("don't call with function without config_dict['{}']".format(manditory_param))
     config_dict.setdefault('file_extension', '.gpg')
     config_dict.setdefault('debug_mode', 'true')
+    config_dict.setdefault('password', 'P@ssW@rd!!!')
 
     if clean_base and os.path.exists(config_dict['base_folder']):
         shutil.rmtree(config_dict['base_folder'])
@@ -44,7 +49,6 @@ def run_program(config_dict, clean_base = True, clean_target = True):
         config_file.write('{}={}\n'.format(key, value))
     config_file.close()
     cmd = 'python3 encrypt_backup.py {}'.format(config_file_name)
-    print('running:' + cmd)
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     ret_val = p.wait()
     stdout = p.stdout.read().decode('utf-8')
@@ -52,8 +56,15 @@ def run_program(config_dict, clean_base = True, clean_target = True):
     p.stdout.close()
     p.stderr.close()
 
+    if print_failed_run and (ret_val != 0):
+        debug(cmd + ' failed')
+        debug('stdout:' + stdout)
+        debug('stderr:' + stderr)
+
     return {'ret_val':ret_val, 'stdout':stdout, 'stderr':stderr}
 
+def debug(debug_str):
+    sys.stderr.write(debug_str + '\n')
 
 if __name__ == '__main__':
     unittest.main()
