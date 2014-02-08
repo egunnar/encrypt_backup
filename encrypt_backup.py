e914d0d34ed6db7b36a795d700b1e7994ba714c3
Erik Gartz <eegunnar@yahoo.com>
2014-02-08 16:10:10 -0500

    refactoring from pylint on encrypt_backup.py

--- encrypt_backup.py
+++ encrypt_backup.py
@@ -22,7 +22,8 @@ import re
 import sys
 
 CONFIG_FILE = 'encrypt_backup.conf'
-VALID_CONFIG_VARIABLES = {'base_folder', 'target_folder', 'file_extension', 'password', 'debug_mode'}
+VALID_CONFIG_VARIABLES = {'base_folder', 'target_folder', 'file_extension',
+    'password', 'debug_mode'}
 MANDITORY_CONFIG_VARIABLE = {'base_folder', 'target_folder', 'password'}
 DEBUG_MODE = False
 
@@ -30,12 +31,12 @@ def first_run_todo(base_folder, target_folder):
     ''' Do some things that need to be once when the program is run for
     the first time or when certain config file setttings change.'''
 
-    for dir in (base_folder, target_folder):
-        os.makedirs(dir, exist_ok = True)
+    for dir_to_create in (base_folder, target_folder):
+        os.makedirs(dir_to_create, exist_ok=True)
 
     # If there is no git repo create one
     os.chdir(base_folder)
-    if not(os.path.exists('.git')):
+    if not os.path.exists('.git'):
         run('git init')
 
 def main():
@@ -58,15 +59,17 @@ def main():
     files = get_files_to_process()
 
     if (len(files['to_add']) == 0) and len(files['to_delete']) == 0:
-       debug('nothing new to encrypt and copy. exiting...')
-       sys.exit(0)
+        debug('nothing new to encrypt and copy. exiting...')
+        sys.exit(0)
     debug('files to process:{}'.format(files))
-    delete_files(config_values['target_folder'], files['to_delete'], config_values['file_extension'])
-    add_encrypted_files(config_values['target_folder'], files['to_add'], config_values)
+    delete_files(config_values['target_folder'], files['to_delete'],
+        config_values['file_extension'])
+    add_encrypted_files(config_values['target_folder'], files['to_add'],
+        config_values)
 
     # do a git commit only now that all the changes are encrypted
     os.chdir(config_values['base_folder'])
-    for file in (files['to_add'] + files['to_delete']):
+    for file in files['to_add'] + files['to_delete']:
         run('git add {}'.format(file))
     run("git commit -a -m 'commit from encrypt backup program'")
 
@@ -86,13 +89,16 @@ def process_config_file(config_file_name):
             config_name = orig_config_name.lower().strip()
             config_value = config_value.strip()
             if config_name not in VALID_CONFIG_VARIABLES:
-                raise Exception('In configuration file:{} config variable: {} is not recognized'.format(config_file_name, orig_config_name))
+                raise Exception('In configuration file:{} config variable: {}\
+                    is not recognized'.format(config_file_name,
+                    orig_config_name))
             config_values[config_name] = config_value
 
 
     for mand_config_key in MANDITORY_CONFIG_VARIABLE:
         if mand_config_key not in config_values:
-            raise Exception('In configuration file:{} missing varible:{}'.format(config_file_name, mand_config_key))
+            raise Exception('In configuration file:{} missing \
+                varible:{}'.format(config_file_name, mand_config_key))
 
     # set some defaults
     if 'file_extension' not in config_values:
@@ -109,24 +115,27 @@ def add_encrypted_files(target_folder, files_to_add, config_values):
     os.chdir(target_folder)
 
     # Example of working gpg command
-    # echo 'sec3rt p@ssworD' | gpg --batch --passphrase-fd 0 --output secert3.txt.gpg --symmetric secert.txt
-    command_to_encrypt = "echo '{0}' | gpg --batch --passphrase-fd 0 --output {{out_file}} --symmetric {{in_file}}".format(config_values['password'])
+    # echo 'sec3rt p@ssworD' | gpg --batch --passphrase-fd 0 --output \
+    #   secert3.txt.gpg --symmetric secert.txt
+    command_to_encrypt = "echo '{0}' | gpg --batch --passphrase-fd 0 --output \
+        {{out_file}} --symmetric {{in_file}}".format(config_values['password'])
 
     for file in files_to_add:
 
         # make the parent folder if it isn't there
-        out_file = config_values['target_folder'] + os.path.sep + file + config_values['file_extension']
+        out_file = '{}/{}{}'.format(config_values['target_folder'], file,
+            config_values['file_extension'])
         out_folder = os.path.dirname(out_file)
-        os.makedirs(out_folder, exist_ok = True)
+        os.makedirs(out_folder, exist_ok=True)
 
         # gpg won't overwrite a file. in an older version of the file
         # exists remove it first
         if os.path.exists(out_file):
             os.unlink(out_file)
 
-        in_file =  config_values['base_folder'] + os.path.sep + file
+        in_file = '{}/{}'.format(config_values['base_folder'], file)
 
-        run(command_to_encrypt.format(in_file = in_file, out_file = out_file))
+        run(command_to_encrypt.format(in_file=in_file, out_file=out_file))
 
 
 def delete_files(target_folder, files_to_delete, file_extension):
@@ -151,7 +160,8 @@ def delete_folders(target_folder, all_folders_to_delete):
         folder_len = len(long_path)
         for i in range(folder_len):
 
-            folder_to_delete = target_folder + '/' + '/'.join(long_path[:folder_len - i])
+            folder_to_delete = '{}/{}'.format(target_folder,
+                '/'.join(long_path[:folder_len - i]))
             try:
                 os.rmdir(folder_to_delete)
             except OSError:
@@ -188,19 +198,20 @@ def run(cmd):
     if the program failed.'''
 
     debug('running shell command:{}'.format(cmd))
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        shell=True)
     ret_val = p.wait()
     stdout = p.stdout.read().decode('utf-8')
     stderr = p.stderr.read().decode('utf-8')
     p.stdout.close()
     p.stderr.close()
-    if (ret_val != 0):
-        sys.stderr.write('Error with command:%s\nstdout:%s\nstderr:%s\n' % (cmd, stdout, stderr))
+    if ret_val != 0:
+        sys.stderr.write('Error with command:%s\nstdout:%s\n\
+            stderr:%s\n'.format(cmd, stdout, stderr))
         sys.exit(ret_val)
     return {'stdout':stdout, 'stderr':stderr}
 
 def debug(input_str):
-    global DEBUG_MODE
     if DEBUG_MODE:
         sys.stderr.write(input_str + '\n')
 
